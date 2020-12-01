4c0180e62166e798d529c08ac49ee80e34c71972
Fabian Ehlert <fabian@fabianehlert.com>
2020-12-01 10:33:04 +0100

    Add initial package structure

---
+++ 2020/Package.swift
@@ -0,0 +1,11 @@
+// swift-tools-version:5.3
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "2020",
+    targets: [
+        .target(name: "Day01", dependencies: [])
+    ]
+)
---
+++ 2020/Sources/Day01/input.swift
@@ -0,0 +1,10 @@
+import Foundation
+
+let input1: [Int] = [
+    1721,
+    979,
+    366,
+    299,
+    675,
+    1456
+]
